934330073768e7b3071c27d02b2acd09eb04fa04
Anshika Kharb <anshika2281.be21@chitkara.edu.in>
2024-05-01 10:08:53 +0530

    npm init and npm i done

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 console.log("server will run here");
 console.log(new Date());
+
+
+// create a git repo
+// add .readme .gitignore FileSystem
+// copy the URL
+// create a new folder on your machine
+// git clone "url"
+// open project in vscode
+// npm init -y
+// add new file AudioParamMap.js with some console statements
+// npm i 
+// git add . 
+// git commit -m "message to add file"
+// git push
